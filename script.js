9b8b728b58aaa55b0378e7edb35f71546a05a67c
diegoxwo <diegovargasmiranda@gmail.com>
2022-09-02 12:50:17 -0500

    Define starter functions and variables

---
+++ script.js
@@ -0,0 +1,22 @@
+//Define a global variable that contains the game choices
+const choices = ["rock", "paper", "scissors"]
+
+//Define a function to play the game for 5 rounds based on the console
+function game() {
+
+}
+
+//Define a function to play one round
+function playRound() {
+    const playerSelection =
+}
+
+//Get the input from the player
+function playerChoice() {
+    
+}
+
+//Get a random input from the computer
+function computerChoice() {
+    
+}
